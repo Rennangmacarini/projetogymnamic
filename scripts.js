b9255658a7bf6beadb8bb54908ea1c44e753d9e4
Rennan Macarini <rennangmacarini@gmail.com>
2022-12-21 10:00:36 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const button = document.querySelector(".h2")
+const visibility = document.querySelector(".p")
+button.addEventListener("click", function(){
+    visibility.classList.toggle("hide")
+})
+
+const buttonOne = document.querySelector(".h2One")
+const visibilityOne = document.querySelector(".pOne")
+buttonOne.addEventListener("click", function(){
+    visibilityOne.classList.toggle("hide")
+})
+
+const buttonTwo = document.querySelector(".h2Two")
+const visibilityTwo = document.querySelector(".pTwo")
+buttonTwo.addEventListener("click", function(){
+    visibilityTwo.classList.toggle("hide")
+})
+
+const buttonThree = document.querySelector(".h2Three")
+const visibilityThree = document.querySelector(".pThree")
+buttonThree.addEventListener("click", function(){
+    visibilityThree.classList.toggle("hide")
+})
+
+const buttonFour = document.querySelector(".h2Four")
+const visibilityFour = document.querySelector(".pFour")
+buttonFour.addEventListener("click", function(){
+    visibilityFour.classList.toggle("hide")
+})
+
+const buttonFive = document.querySelector(".h2Five")
+const visibilityFive = document.querySelector(".pFive")
+buttonFive.addEventListener("click", function(){
+    visibilityFive.classList.toggle("hide")
+})
